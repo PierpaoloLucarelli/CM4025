7d8e0ada2f79d6316b37ccc0a7bcd41dda086737
Pierpaolo <plucarelli.94@gmail.com>
2018-02-19 12:37:57 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html'); 
+});
+
+server.listen(8000,function(){ // Listens to port 8000
+    console.log('Listening on '+server.address().port);
+});
